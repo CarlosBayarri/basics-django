2cb66b678da243eddc4bd2a82ad1d9120f3b5cab
carlosbayarri <carlobr7@gmail.com>
2020-01-27 16:48:46 +0100

    templates

--- xsite/polls/views.py
+++ xsite/polls/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question, Choice
 # Create your views here.
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
     output = ", ".join(q.question_text for q in latest_questions)
-    return HttpResponse(output)
+    #template = loader.get_template('polls/index.html')
+    context = {
+        'latest_questions': latest_questions
+    }
+    return render(request, 'polls/index.html', context)
+    #return HttpResponse(template.render(context))
+    #return HttpResponse(output)
 
 def detail(request, question_id):
-    return HttpResponse("This is a detail view of the question: %s" % question_id)
+    #question = Question.objects.get(pk = question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    #return HttpResponse("This is a detail view of the question: %s" % question_id)
 
 def results(request, question_id):
     return HttpResponse("These are the results of the question: %s" % question_id)
